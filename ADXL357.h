fe66ff71cb705c2d4d50b5b687ea3576b6271eb6
rhxie <38510716+rhxie@users.noreply.github.com>
2018-08-07 13:50:53 +0000

    Add comments
    add comments

--- ADXL357.h
+++ ADXL357.h
@@ -27,11 +27,11 @@
 #define DEVID_AD		0x00
 #define DEVID_MST		0x01
 #define PARTID			0x02
-#define REVID			0x03
+#define REVID			  0x03
 #define STATUS			0x04
-#define FIFO_ENTRIES	        0x05
-#define TEMP2			0x06
-#define TEMP1			0x07
+#define FIFO_ENTRIES	        0x05   // new comments
+#define TEMP2			  0x06
+#define TEMP1			  0x07
 #define XDATA3			0x08
 #define XDATA2			0x09
 #define XDATA1			0x0A
@@ -48,16 +48,16 @@
 #define OFFSET_Y_L		0x21
 #define OFFSET_Z_H		0x22
 #define OFFSET_Z_L		0x23
-#define ACT_EN			0x24
+#define ACT_EN			  0x24
 #define ACT_THRESH_H	        0x25
 #define ACT_THRESH_L	        0x26
 #define ACT_COUNT		0x27
 #define FILTER			0x28
 #define FIFO_SAMPLES	        0x29
 #define INT_MAP			0x2A
-#define SYNC			0x2B
-#define RANGE			0x2C
+#define SYNC			  0x2B
+#define RANGE			  0x2C
 #define POWER_CTL		0x2D
 #define SELF_TEST		0x2E
-#define RESET			0x2F
+#define RESET			  0x2F
 
